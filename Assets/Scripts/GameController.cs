7162aa24532a1e1bf394965865ff90f84265bdb4
armour07 <13202511825@163.com>
2021-04-15 23:25:14 +0800

    工程提交

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,392 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class GameController : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler{
+
+	public Vector2 MapSize;
+	public Vector2 GameSize;
+	public Grid[,] Grids;
+	public GameObject GridGo;
+	public RectTransform GamePanel;
+	private int score;
+	public int Score {
+		get {
+			return score;
+		}
+		set
+        {
+			score = value;
+			TxtScore.text = string.Format("分数：" + score);
+        }
+	}
+	public Text TxtScore;
+
+
+	void Awake()
+    {
+		Grids = new Grid[(int)GameSize.x, (int)GameSize.y];
+		GamePanel.sizeDelta = MapSize;
+		GamePanel.anchoredPosition = -MapSize / 2;
+    }
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < GameSize.x; i++)
+        {
+            for (int j = 0; j < GameSize.y; j++)
+            {
+				var go = Instantiate(GridGo);
+				go.name = string.Format("({0},{1})", i, j);
+				go.transform.SetParent(GamePanel, false);
+				Grids[i, j] = go.GetComponent<Grid>();
+			}
+        }
+		Restart();
+	}
+
+	public void Restart()
+    {
+		Score = 0;
+		var grid_size = new Vector2(MapSize.x / GameSize.x, MapSize.y / GameSize.y);
+		for (int i = 0; i < GameSize.x; i++)
+		{
+			for (int j = 0; j < GameSize.y; j++)
+			{
+				var grid = Grids[i, j];
+				grid.Size = grid_size;
+				grid.Pos = new Vector2(i, j);
+				grid.Num = 0;
+			}
+		}
+		AddNum();
+		AddNum();
+	}
+
+	public int[] RangeNums = { 2, 4 };
+
+	public void AddNum()
+    {
+		var zero_grid_list = new List<Grid>();
+		for (int i = 0; i < GameSize.x; i++)
+		{
+			for (int j = 0; j < GameSize.y; j++)
+			{
+				var grid = Grids[i, j];
+				if (grid.Num == 0)
+                {
+					zero_grid_list.Add(grid);
+				}
+			}
+		}
+		if(zero_grid_list.Count == 0)
+        {
+			return;
+        }
+		var pos = Random.Range(0, zero_grid_list.Count);
+		zero_grid_list[pos].Num = RangeNums[Random.Range(0, RangeNums.Length)];
+	}
+
+	bool CheckOver()
+    {
+        for (int i = 0; i < GameSize.x; i++)
+        {
+            for (int j = 0; j < GameSize.y; j++)
+            {
+                var grid = Grids[i, j];
+                for (int k = -1; k <= 1; k++)
+                {
+                    if (k != 0)
+                    {
+						if(i + k >= 0 && i + k < GameSize.x)
+                        {
+							var lr_grid = Grids[i + k, j];
+							if(lr_grid.Num == grid.Num)
+                            {
+								return false;
+                            }
+                        }
+						if (j + k >= 0 && j + k < GameSize.y)
+						{
+							var tb_grid = Grids[i, j + k];
+							if (tb_grid.Num == grid.Num)
+							{
+								return false;
+							}
+						}
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+	void LeftDrag()
+    {
+		AllUnLock();
+		var have_move = false;
+		for (int i = 1; i < GameSize.x; i++)
+		{
+			for (int j = 0; j < GameSize.y; j++)
+			{
+				var grid = Grids[i, j];
+				if(grid.Num > 0)
+                {
+					for (int k = (int)grid.Pos.x - 1; k >= 0; k--)
+					{
+						var _grid = Grids[k, j];
+						if (_grid.IsLock)
+						{
+							break;
+						}
+						if (_grid.Num == 0)
+						{
+							_grid.Num = grid.Num;
+							grid.Num = 0;
+							grid = _grid;
+							have_move = true;
+						}
+                        else if(_grid.Num == grid.Num)
+                        {
+							var tar_num = grid.Num + _grid.Num;
+							_grid.Num = tar_num;
+							Score = Score + tar_num;
+							grid.Num = 0;
+							_grid.IsLock = true;
+							have_move = true;
+							break;
+						}
+                        else if(_grid.Num != grid.Num)
+                        {
+							break;
+                        }
+					}
+                }
+			}
+		}
+        if (have_move)
+        {
+			AddNum();
+        }
+		if (CheckOver() == true)
+		{
+			print("游戏结束");
+		}
+	}
+
+	void RightDrag()
+	{
+		AllUnLock();
+		var have_move = false;
+		for (int i = (int)GameSize.x - 1; i >= 0; i--)
+		{
+			for (int j = 0; j < GameSize.y; j++)
+			{
+				var grid = Grids[i, j];
+				if (grid.Num > 0)
+				{
+					for (int k = (int)grid.Pos.x + 1; k < GameSize.x; k++)
+					{
+						var _grid = Grids[k, j];
+						if (_grid.IsLock)
+						{
+							break;
+						}
+						if (_grid.Num == 0)
+						{
+							_grid.Num = grid.Num;
+							grid.Num = 0;
+							grid = _grid;
+							have_move = true;
+
+						}
+						else if (_grid.Num == grid.Num)
+						{
+							var tar_num = grid.Num + _grid.Num;
+							_grid.Num = tar_num;
+							Score = Score + tar_num;
+							grid.Num = 0;
+							_grid.IsLock = true;
+							have_move = true;
+							break;
+						}
+						else if (_grid.Num != grid.Num)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (have_move)
+		{
+			AddNum();
+		}
+		if (CheckOver() == true)
+		{
+			print("游戏结束");
+		}
+	}
+	void TopDrag()
+	{
+		AllUnLock();
+		var have_move = false;
+		 for (int j = (int)GameSize.y - 1; j >= 0; j--)
+		{
+			for (int i = 0; i < GameSize.x; i++)
+			{
+				var grid = Grids[i, j];
+				if (grid.Num > 0)
+				{
+					for (int k = (int)grid.Pos.y + 1; k < GameSize.y; k++)
+					{
+						var _grid = Grids[i, k];
+						if (_grid.IsLock)
+						{
+							break;
+						}
+						if (_grid.Num == 0)
+						{
+							_grid.Num = grid.Num;
+							grid.Num = 0;
+							grid = _grid;
+							have_move = true;
+
+						}
+						else if (_grid.Num == grid.Num)
+						{
+							var tar_num = grid.Num + _grid.Num;
+							_grid.Num = tar_num;
+							Score = Score + tar_num;
+							grid.Num = 0;
+							_grid.IsLock = true;
+							have_move = true;
+							break;
+						}
+						else if (_grid.Num != grid.Num)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (have_move)
+		{
+			AddNum();
+		}
+		if (CheckOver() == true)
+		{
+			print("游戏结束");
+		}
+	}
+	void DownDrag()
+	{
+		AllUnLock();
+		var add_num = false;
+		 for (int j = 1; j < GameSize.y; j++)
+			{
+			for (int i = 0; i < GameSize.x; i++)
+			{
+				var grid = Grids[i, j];
+				if (grid.Num > 0)
+				{
+					for (int k = (int)grid.Pos.y - 1; k >= 0; k--)
+					{
+						var _grid = Grids[i, k];
+                        if (_grid.IsLock)
+                        {
+							break;
+                        }
+						if (_grid.Num == 0)
+						{
+							_grid.Num = grid.Num;
+							grid.Num = 0;
+							grid = _grid;
+							add_num = true;
+						}
+						else if (_grid.Num == grid.Num)
+						{
+							var tar_num = grid.Num + _grid.Num;
+							_grid.Num = tar_num;
+							Score = Score + tar_num;
+							grid.Num = 0;
+							_grid.IsLock = true;
+							add_num = true;
+							break;
+						}
+						else if (_grid.Num != grid.Num)
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (add_num)
+		{
+			AddNum();
+		}
+		if (CheckOver() == true)
+		{
+			print("游戏结束");
+		}
+	}
+	void AllUnLock()
+    {
+		for (int i = 0; i < GameSize.x; i++)
+		{
+			for (int j = 0; j < GameSize.y; j++)
+			{
+				var grid = Grids[i, j];
+				grid.IsLock = false;
+			}
+		}
+	}
+
+	private Vector2 beginDragPos;
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		beginDragPos = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		var pos = eventData.position - beginDragPos;
+		var abs_x = Mathf.Abs(pos.x);
+		var abs_y = Mathf.Abs(pos.y);
+		var max = Mathf.Max(abs_x, abs_y);
+		if(max < 30)
+        {
+			return;
+        }
+		if(Mathf.Abs(pos.x) > Mathf.Abs(pos.y))
+        {
+			if(pos.x > 0)
+            {
+				RightDrag();
+            }
+            else
+            {
+				LeftDrag();
+			}
+        }
+        else
+        {
+			if (pos.y > 0)
+			{
+				TopDrag();
+			}
+			else
+			{
+				DownDrag();
+			}
+		}
+	}
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	public Image ImgBg;
+	public Vector2 Size;
+	private Vector2 pos;
+	public Vector2 Pos
+    {
+        get
+        {
+			return pos;
+        }
+        set
+        {
+			pos = value;
+			transform.localPosition = new Vector2(pos.x * Size.x, pos.y * Size.y);
+			transform.GetComponent<RectTransform>().sizeDelta = Size;
+		}
+	}
+	private int num = 0;
+	public int Num
+    {
+		get
+		{
+			return num;
+		}
+		set
+		{
+			num = value;
+			if (num > 0)
+            {
+				TxtNum.text = num.ToString();
+            }
+            else
+            {
+				TxtNum.text = "";
+            }
+			
+			var show_color = Colors.Find(c => c.Num == (int)num);
+			ImgBg.color = show_color.BgColor;
+			TxtNum.color = show_color.NumColor;
+
+		}
+    }
+	public Text TxtNum;
+	public bool IsLock;
+
+	[System.Serializable]
+	public struct ShowColor{
+		public int Num;
+		public Color BgColor;
+		public Color NumColor;
+	}
+	public List<ShowColor> Colors;
+}
